84aed97632c42832738af491f11fe7ac500605b1
Arun Gopinath <agnath18K@gmail.com>
2024-10-31 05:31:39 +0530

    update example

--- example/blaze_engine_example.dart
+++ example/blaze_engine_example.dart
@@ -2,6 +2,9 @@ import 'package:blaze_engine/blaze_engine.dart';
 
 void main() async {
   final downloader = BlazeDownloader(
+      allowResume: true,
+      sequentialDownload: false,
+      enableWorkerPooling: true,
       downloadUrl:
           'https://releases.ubuntu.com/jammy/ubuntu-22.04.5-desktop-amd64.iso',
       customDirectory: 'download');
